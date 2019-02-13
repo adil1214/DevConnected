5b62677f0c8a895c2590a648288ea3670cae2363
Adil <adil1350@gmail.com>
2019-02-13 12:24:55 +0100

    minor fix

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -30,7 +30,7 @@ class Dashboard extends Component {
 				dashboardContent = (
 					<div>
 						<p className="lead text-muted">
-							Welcome <Link to={`/profile/profile/${profile.handle}`}>{user.name}</Link>{' '}
+							Welcome <Link to={`/profile/handle/${profile.handle}`}>{user.name}</Link>{' '}
 						</p>
 						<ProfileActions />
 						<Experience experience={profile.experience} />
